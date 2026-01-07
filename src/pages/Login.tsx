eb10917afaf8eff5735d3bc8b6561f6462161eb0
Ariel G <ariel.garces@uisek.edu.ec>
2026-01-07 11:14:13 -0500

    Finalizacion laboratorio 10 MP

---
+++ src/pages/Login.tsx
@@ -0,0 +1,79 @@
+import { IonButton, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonText, IonTitle, IonToolbar } from "@ionic/react";
+import { logoGithub } from "ionicons/icons";
+import './Login.css';
+import { useState } from "react";
+import AuthService from "../services/AuthService";
+
+const Login: React.FC = () => {
+    const [username, setUsername] = useState('');
+    const [token, setToken] = useState('');
+    const [error, setError] = useState('');
+
+    const handlelogin = (e: React.FormEvent) => {
+        e.preventDefault();
+        setError('');
+        if (!username || !token) {
+            setError('Por favor, ingresa tu usuario y token de Github');
+            return;
+        }
+        const success = AuthService.login(username, token);
+        if (success) {
+            window.location.href = '/tab1';
+        } else {
+            setError('Error al iniciar sesión.');
+        }
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Iniciar Sesión</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen className="ion-padding">
+                <div className="login-container">
+                    <IonIcon icon={logoGithub} className="login-logo"/>
+                    <h1>Inicio de sesión a Github</h1>
+                    <form className="login-form" onSubmit={handlelogin}>
+                        <IonInput
+                         className="login-field"
+                         label="Usuario de github"
+                         labelPlacement="floating"
+                         fill="outline"
+                         type="text"
+                         value={username}
+                         onIonInput={e => setUsername(e.detail.value!)}
+                         required
+                         />
+
+                        <IonInput
+                         className="login-field"
+                         label="Token de Github"
+                         labelPlacement="floating"
+                         fill="outline"
+                         type="password"
+                         value={token}
+                         onIonInput={e => setToken(e.detail.value!)}
+                         required
+                         />
+
+                         {error && (
+                            <IonText color="danger" className="error-message">
+                                {error}
+                            </IonText>
+                         )}
+
+                         <IonButton expand="block" type="submit">
+                            Iniciar Sesión
+                         </IonButton>
+                         <IonText color="medium" className="login-hint">
+                            <p>Ingresa tu usuario y tu Token de Github</p>
+                         </IonText>
+                    </form>
+                </div>
+            </IonContent>
+        </IonPage>
+    );
+}
+export default Login;
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,13 +1,17 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import './Tab3.css';
 import { useState } from 'react';
 import { UserInfo } from '../interfaces/UserInfo';
 import { getUserInfo } from '../services/GithubService';
+import { logOutOutline } from 'ionicons/icons';
+import { useHistory } from 'react-router';
+import AuthService from '../services/AuthService';
 
 
 const Tab3: React.FC = () => {
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+  const history = useHistory();
 
   const loadUserInfo = async () => {
     const info = await getUserInfo();
@@ -17,6 +21,11 @@ const Tab3: React.FC = () => {
   useIonViewDidEnter(() => {
     loadUserInfo();
   })
+
+  const handleLogout = () => {
+    AuthService.logout();
+    history.replace('/login');
+  }
 
   return (
     <IonPage>
@@ -40,6 +49,11 @@ const Tab3: React.FC = () => {
 
       <IonCardContent>{userInfo?.bio}</IonCardContent>
     </IonCard>
+
+    <IonButton expand="block" color="danger" onClick={handleLogout}>
+      <IonIcon slot="start" icon={logOutOutline} />
+      Cerrar Sesión
+    </IonButton>
       </IonContent>
     </IonPage>
   );
